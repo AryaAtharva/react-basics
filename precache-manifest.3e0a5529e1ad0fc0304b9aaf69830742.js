3261ba2ed2928de8051a68901bda6b6afa1000af
Arya <arya.atharva6@gmail.com>
2020-09-30 12:12:44 +0530

    Updates

---
+++ precache-manifest.3e0a5529e1ad0fc0304b9aaf69830742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ceb907bf352d231402e8cd5365b1ee",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "6db4915930a4694fb45c",
+    "url": "/react-basics/static/css/main.d867d30e.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/react-basics/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db4915930a4694fb45c",
+    "url": "/react-basics/static/js/main.48460e58.chunk.js"
+  },
+  {
+    "revision": "5d170ef11314c90c8b16",
+    "url": "/react-basics/static/js/runtime-main.4a78090c.js"
+  }
+]);
